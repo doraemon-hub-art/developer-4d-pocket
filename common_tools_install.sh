36dc822d749fbe0b95d64838e0809b2992740e09
doraemon-hub-art <1660219734@qq.com>
2026-03-09 00:02:57 +0800

    feat(common_tools_install): add new script

---
+++ common_tools_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# node 版本工具
+echo "nvm 安装"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# nvm install 20：安装最新的 Node 20.x。
+# 切换版本： nvm use 20。
+# nvm alias default 20：设置默认启动版本
+
+
